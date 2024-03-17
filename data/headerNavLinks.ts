37447b19f515405cb299809e3bb42b181fad21b9
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-03-18 01:21:31 +0900

    refactor: about 페이지 은닉

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,7 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Posts' },
   { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  // { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
